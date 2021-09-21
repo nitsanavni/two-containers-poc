5bbad74eff1b928d550d7b3b0c0ddc752485fa39
Nitsan Avni <nitsan@papayaglobal.com>
2021-09-21 15:54:16 +0300

    format

--- rerun.sh
+++ rerun.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 kubectl delete pod two-containers
-parallel --linebuffer --tag docker build -f Dockerfile.{} . -t {} ::: reader writer
+parallel --linebuffer --tag \
+    docker build -f Dockerfile.{} . -t {} ::: \
+        reader writer
 kubectl apply -f two-container-pod.yaml
-parallel --linebuffer --tag retry -n 7 -- kubectl logs -f two-containers -c {} ::: reader writer
+parallel --linebuffer --tag \
+    retry -n 7 -- \
+        kubectl logs -f two-containers -c {} ::: \
+            reader writer
