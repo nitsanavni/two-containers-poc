3e1f2294cd6dbd54da684af511756919190ce2d1
Nitsan Avni <nitsan@papayaglobal.com>
2021-09-21 12:43:46 +0300

    reader.mjs

---
+++ reader.mjs
@@ -0,0 +1,10 @@
+let i = 0;
+
+while (true) {
+    if (/13/.test(String(await nothrow($`cat /vol-reader/file`)))) {
+        console.log(`reader found our 13! iteration ${i}`);
+        process.exit(0);
+    }
+    await sleep(40);
+    i++;
+}
