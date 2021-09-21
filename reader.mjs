24431cf56eeaaf8f852a742d2bc457a47206fe20
Daniel Habib <daniel@danielhabib.co.il>
2021-09-21 13:18:14 +0300

    mount as vol readonly

--- reader.mjs
+++ reader.mjs
@@ -1,7 +1,7 @@
 let i = 0;
 
 while (true) {
-    if (/13/.test(String(await nothrow($`cat /vol-reader/file`)))) {
+    if (/13/.test(String(await nothrow($`cat /vol/file`)))) {
         console.log(`reader found our 13! iteration ${i}`);
         process.exit(0);
     }
