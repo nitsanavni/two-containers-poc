08f172bb5be29f04fe334445029c55d551113895
Nitsan Avni <nitsan@papayaglobal.com>
2021-09-21 12:39:17 +0300

    /tmp/vol -> /vol

--- writer.mjs
+++ writer.mjs
@@ -1,7 +1,9 @@
-$`mkdir -p /tmp/vol`;
-$`rm -rf /tmp/vol/*`;
+$`mkdir -p /vol`;
+$`rm -rf /vol/*`;
 
 for (let i = 0; i < 15; i++) {
     await sleep(100);
-    await $`echo ${i} >> /tmp/vol/file`;
+    await $`echo ${i} >> /vol/file`;
 }
+
+await $`cat /vol/file`;
